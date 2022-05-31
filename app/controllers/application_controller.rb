86f80ce44d2f5ec5d540f4f25c4d964bb1dab13f
Brody Losh <brodylosh@gmail.com>
2022-05-30 22:36:40 -0400

    commit tree 42a079a3d3d53f6586982acab3ea2813f6940285

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   # add routes
+  get '/bakeries' do
+    bakery = Bakery.all
+    bakery.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.all.sort_by(&:price).reverse
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked_goods = BakedGood.all.order(:price).reverse
+    expensive = baked_goods.first
+    expensive.to_json
+  end
 
 end
